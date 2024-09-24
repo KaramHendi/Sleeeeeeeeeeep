e8e0c67f6b059da0592a4235b064326caddc510a
KARAMHENDIZD <karamhendizd10@gmail.com>
2024-09-24 11:49:49 +0300

    Third commit

--- app/src/main/java/com/example/karam/MainActivity.java
+++ app/src/main/java/com/example/karam/MainActivity.java
@@ -1,39 +1,68 @@
 package com.example.karam;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private int i = 0;
-    private TextView txtt;
-    private Thread counterThread;
 
+    private TextView timerTextView;
+    private Button startButton;
+    private Button stopButton;
+    private Button resetButton;
+
+    private boolean isRunning = false;
+    private int seconds = 0;
+    private Handler handler = new Handler();
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (isRunning) {
+                seconds++;
+                int minutes = seconds / 60;
+                int secs = seconds % 60;
+                timerTextView.setText(String.format("%02d:%02d", minutes, secs));
+                handler.postDelayed(this, 1000);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtt = findViewById(R.id.Txt);
-    }
 
-    public void startCount(View view) {
-        counterThread = new Thread(() -> {
-            try {
-                while (true) {
-                    i++;
-                    txtt.setText(i + "");
-                    Thread.sleep(1000);
-                }
-            } catch (Exception e) {}
+        timerTextView = findViewById(R.id.timerTextView);
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = true;
+                handler.post(runnable);
+            }
         });
-        counterThread.start();
-    }
 
-    public void stopCount(View view) {
-        counterThread.interrupt();
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = false;
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = false;
+                seconds = 0;
+                timerTextView.setText("00:00");
+            }
+        });
     }
 }
